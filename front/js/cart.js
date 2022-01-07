63f0436f8bdfbdac2a9d437d995826771c95b3f8
Maki-Alex <apj.grosjean@gmail.com>
2022-01-07 17:32:49 +0100

    optimisation code & commentaire sur ancienne methode

--- front/js/cart.js
+++ front/js/cart.js
@@ -47,6 +47,8 @@ const removeStorage = (product) => {
 // switchCase for id.img
 let img;
 
+const host = "http://localhost:3000/api/products";
+
 if (!storage) {
 
     // console.log("panier vide");
@@ -54,44 +56,71 @@ if (!storage) {
 } else {
 
     for (const identification of items) {
+        //************************************ test code ******************************/
+        
+        const getImg = () => {
+            fetch(host)
+            .then((response) => response.json())
+            .then((data) => {
+                    for (i = 0; i < data.length; i++) {
+                        // console.log(data[i]._id);
+                        if (identification.id == data[i]._id) {
+                            img = {imgKanap: data[i].imageUrl};
+                            let item = storage.filter (element => { return (element.id == identification.id && element.color == identification.color)});
+                            // // console.log(img);
+                            if(item.length > 0) {
+                                let pos = storage.findIndex((element) => (element.id == identification.id && element.color == identification.color));
+                                items[pos] = item[0];
 
-        switch (identification.id) {
-            case '107fb5b75607497b96722bda5b504926':
-                identification.img = "../../back/images/kanap01.jpeg";
-                break;
-
-            case '415b7cacb65d43b2b5c1ff70f3393ad1':
-                identification.img = "../../back/images/kanap02.jpeg";
-                break;
-
-            case '055743915a544fde83cfdfc904935ee7':
-                identification.img = "../../back/images/kanap03.jpeg";
-                break;
-
-            case 'a557292fe5814ea2b15c6ef4bd73ed83':
-                identification.img = "../../back/images/kanap04.jpeg";
-                break;
-
-            case '8906dfda133f4c20a9d0e34f18adcf06':
-                identification.img = "../../back/images/kanap05.jpeg";
-                break;
-
-            case '77711f0e466b4ddf953f677d30b0efc9':
-                identification.img = "../../back/images/kanap06.jpeg";
-                break;
-
-            case '034707184e8e4eefb46400b5a3774b5f':
-                identification.img = "../../back/images/kanap07.jpeg";
-                break;
-
-            case 'a6ec5b49bd164d7fbe10f37b6363f9fb':
-                identification.img = "../../back/images/kanap08.jpeg";
-                break;
-
-            default:
-                console.log("cart empty")
-                break;
+                                let result = Object.assign(items[pos], img)
+                                // console.log(result)
+                                localStorage.setItem("cart", JSON.stringify(storage));
+                            }
+                }
+                }
+            })
         }
+        getImg()
+
+        //************************************ fin test code ******************************/
+
+        // switch (identification.id) {
+        //     case '107fb5b75607497b96722bda5b504926':
+        //         identification.img = "../../back/images/kanap01.jpeg";
+        //         break;
+
+        //     case '415b7cacb65d43b2b5c1ff70f3393ad1':
+        //         identification.img = "../../back/images/kanap02.jpeg";
+        //         break;
+
+        //     case '055743915a544fde83cfdfc904935ee7':
+        //         identification.img = "../../back/images/kanap03.jpeg";
+        //         break;
+
+        //     case 'a557292fe5814ea2b15c6ef4bd73ed83':
+        //         identification.img = "../../back/images/kanap04.jpeg";
+        //         break;
+
+        //     case '8906dfda133f4c20a9d0e34f18adcf06':
+        //         identification.img = "../../back/images/kanap05.jpeg";
+        //         break;
+
+        //     case '77711f0e466b4ddf953f677d30b0efc9':
+        //         identification.img = "../../back/images/kanap06.jpeg";
+        //         break;
+
+        //     case '034707184e8e4eefb46400b5a3774b5f':
+        //         identification.img = "../../back/images/kanap07.jpeg";
+        //         break;
+
+        //     case 'a6ec5b49bd164d7fbe10f37b6363f9fb':
+        //         identification.img = "../../back/images/kanap08.jpeg";
+        //         break;
+
+        //     default:
+        //         console.log("cart empty")
+        //         break;
+        // }
     }
 
 };
@@ -110,7 +139,7 @@ if (storage != null) {
                 `
                 <article class="cart__item" data-id="${element.id}" data-color="${element.color}">
                 <div class="cart__item__img">
-                <img src="${element.img}" alt="Photographie d'un canapé">
+                <img src="${element.imgKanap}" alt="Photographie d'un canapé">
                 </div>
                 <div class="cart__item__content">
                 <div class="cart__item__content__description">
@@ -236,6 +265,11 @@ sumTotal();
 
 
 
+
+
+//************************************ test code  *********************************/
+
+
 //regex & formulaire
 
 let form = document.querySelector(".cart__order__form");
@@ -254,18 +288,22 @@ form.firstName.addEventListener('change', function () {
     validFirstName(this)
 })
 
-// validation du input
-const validFirstName = function (inputFirstName) {
-    const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
-    let errorFirstName = document.querySelector('#firstNameErrorMsg');
+const fieldValidation = function (value, htmlSelector, regex) {
+    const regexName = regex;
+    let errorFirstName = document.querySelector('#' + htmlSelector);
 
-    if (regexName.test(inputFirstName.value)) {
+    if (regexName.test(value)) {
         errorFirstName.innerHTML = 'valide';
-        prenom = form.firstName.value;
         console.log(prenom)
     } else {
         errorFirstName.innerHTML = "non valide";
+        console.log('error input')
     }
+}
+
+// validation du input
+const validFirstName = function (inputFirstName) {
+    fieldValidation(inputFirstName, 'firstNameErrorMsg', /^[a-z][a-z '-.,]{1,31}$|^$/i);
 }
 
 // nom
@@ -277,16 +315,7 @@ form.lastName.addEventListener('change', function () {
 
 // validation du input
 const validLastName = function (inputLastName) {
-    const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
-    let errorLastName = document.querySelector('#lastNameErrorMsg');
-
-    if (regexName.test(inputLastName.value)) {
-        errorLastName.innerHTML = 'valide';
-        nom = form.lastName.value;
-        console.log(nom)
-    } else {
-        errorLastName.innerHTML = "non valide";
-    }
+    fieldValidation(inputLastName, 'lastNameErrorMsg', /^[a-z][a-z '-.,]{1,31}$|^$/i);
 }
 
 // adresse
@@ -298,16 +327,7 @@ form.address.addEventListener('change', function () {
 
 // validation du input
 const validAddress = function (inputAddress) {
-    const regexAddress = /^[#.0-9a-zA-Z\s,-]+$/i;
-    let addressErrorMsg = document.querySelector('#addressErrorMsg');
-
-    if (regexAddress.test(inputAddress.value)) {
-        addressErrorMsg.innerHTML = 'valide';
-        address = form.address.value;
-        console.log(address)
-    } else {
-        addressErrorMsg.innerHTML = "non valide";
-    }
+    fieldValidation(inputAddress, 'addressErrorMsg',  /^[#.0-9a-zA-Z\s,-]+$/i);
 }
 
 // ville
@@ -319,16 +339,7 @@ form.city.addEventListener('change', function () {
 
 // validation du input
 const validCity = function (inputCity) {
-    const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
-    let cityErrorMsg = document.querySelector('#cityErrorMsg');
-
-    if (regexName.test(inputCity.value)) {
-        cityErrorMsg.innerHTML = 'valide';
-        ville = form.city.value;
-        console.log(ville)
-    } else {
-        cityErrorMsg.innerHTML = "non valide";
-    }
+    fieldValidation(inputCity, 'cityErrorMsg', /^[a-z][a-z '-.,]{1,31}$|^$/i);
 }
 
 // mail
@@ -341,16 +352,126 @@ form.email.addEventListener('change', function () {
 // validation du input
 const validEmail = function (inputMail) {
     const regexMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let emailErrorMsg = document.querySelector('#emailErrorMsg');
-
-    if (regexMail.test(inputMail.value)) {
-        emailErrorMsg.innerHTML = 'valide';
-        email = form.email.value;
-        console.log(email)
-    } else {
-        emailErrorMsg.innerHTML = "non valide";
-    }
+    fieldValidation(inputMail, 'emailErrorMsg', regexMail);
 }
+
+//************************************ fin test code ******************************/
+
+// // regex & formulaire
+
+// let form = document.querySelector(".cart__order__form");
+
+// //variable for object
+// let prenom;
+// let nom;
+// let address;
+// let ville;
+// let email;
+
+// // prenom
+
+// // listening modification
+// form.firstName.addEventListener('change', function () {
+//     validFirstName(this)
+// })
+
+// // validation du input
+// const validFirstName = function (inputFirstName) {
+//     const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
+//     let errorFirstName = document.querySelector('#firstNameErrorMsg');
+
+//     if (regexName.test(inputFirstName.value)) {
+//         errorFirstName.innerHTML = 'valide';
+//         prenom = form.firstName.value;
+//         console.log(prenom)
+//     } else {
+//         errorFirstName.innerHTML = "non valide";
+//     }
+// }
+
+// // nom
+
+// // listening modification
+// form.lastName.addEventListener('change', function () {
+//     validLastName(this)
+// })
+
+// // validation du input
+// const validLastName = function (inputLastName) {
+//     const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
+//     let errorLastName = document.querySelector('#lastNameErrorMsg');
+
+//     if (regexName.test(inputLastName.value)) {
+//         errorLastName.innerHTML = 'valide';
+//         nom = form.lastName.value;
+//         console.log(nom)
+//     } else {
+//         errorLastName.innerHTML = "non valide";
+//     }
+// }
+
+// // adresse
+
+// // listening modification
+// form.address.addEventListener('change', function () {
+//     validAddress(this)
+// })
+
+// // validation du input
+// const validAddress = function (inputAddress) {
+//     const regexAddress = /^[#.0-9a-zA-Z\s,-]+$/i;
+//     let addressErrorMsg = document.querySelector('#addressErrorMsg');
+
+//     if (regexAddress.test(inputAddress.value)) {
+//         addressErrorMsg.innerHTML = 'valide';
+//         address = form.address.value;
+//         console.log(address)
+//     } else {
+//         addressErrorMsg.innerHTML = "non valide";
+//     }
+// }
+
+// // ville
+
+// // listening modification
+// form.city.addEventListener('change', function () {
+//     validCity(this)
+// })
+
+// // validation du input
+// const validCity = function (inputCity) {
+//     const regexName = /^[a-z][a-z '-.,]{1,31}$|^$/i;
+//     let cityErrorMsg = document.querySelector('#cityErrorMsg');
+
+//     if (regexName.test(inputCity.value)) {
+//         cityErrorMsg.innerHTML = 'valide';
+//         ville = form.city.value;
+//         console.log(ville)
+//     } else {
+//         cityErrorMsg.innerHTML = "non valide";
+//     }
+// }
+
+// // mail
+
+// // listening modification
+// form.email.addEventListener('change', function () {
+//     validEmail(this)
+// })
+
+// // validation du input
+// const validEmail = function (inputMail) {
+//     const regexMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     let emailErrorMsg = document.querySelector('#emailErrorMsg');
+
+//     if (regexMail.test(inputMail.value)) {
+//         emailErrorMsg.innerHTML = 'valide';
+//         email = form.email.value;
+//         console.log(email)
+//     } else {
+//         emailErrorMsg.innerHTML = "non valide";
+//     }
+// }
 
 //action on submit
 const commande = document.querySelector('#order');
@@ -390,11 +511,21 @@ if (storage != null) {
         makeJson();
         let jsonData = makeJson();
 
-        if (makeJson()["contact"].firstName != null &&
-            makeJson()["contact"].lastName != null &&
-            makeJson()["contact"].address != null &&
-            makeJson()["contact"].city != null &&
-            makeJson()["contact"].email != null ) {
+        //************************************  code modifié ******************************/
+
+        const getField = (field) => {
+            return makeJson()["contact"][field];
+        }
+
+        if (getField('firstName', 'lastName', 'address', 'city', 'email') != null) {
+
+            //************************************fin code modifié ******************************/
+
+            // if (makeJson()["contact"].firstName != null &&
+            //     makeJson()["contact"].lastName != null &&
+            //     makeJson()["contact"].address != null &&
+            //     makeJson()["contact"].city != null &&
+            //     makeJson()["contact"].email != null ) {
 
             let myInit = {
                 method: "POST",
@@ -405,15 +536,16 @@ if (storage != null) {
                 }
             };
 
+            //************************************  code modifié ******************************/
+
             let commandFetch = () => {
                 fetch(postUrl, myInit)
                     .then((response) => response.json())
                     .then((data) => {
                         console.log(data);
                         localStorage.clear();
-                        localStorage.setItem("orderId", data.orderId);
-
-                        document.location.href = "confirmation.html";
+                        // localStorage.setItem("orderId", data.orderId);
+                        document.location.href = `confirmation.html?id=${data.orderId}`;
                     })
                     .catch((err) => {
                         alert("Problème avec fetch : " + err.message);
@@ -422,4 +554,26 @@ if (storage != null) {
             commandFetch();
         }
     }
-};
+
+}
+//************************************fin code modifié ******************************/
+
+// let commandFetch = () => {
+//     fetch(postUrl, myInit)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//             localStorage.clear();
+//             localStorage.setItem("orderId", data.orderId);
+
+//             document.location.href = "confirmation.html";
+//         })
+//         .catch((err) => {
+//             alert("Problème avec fetch : " + err.message);
+//         });
+// }
+// commandFetch();
+
+//         }
+//     }
+// }
